8719861680f7dc9c95b8e69722cf99af376757f3
J Kimball <jeffkimballis@gmail.com>
2024-02-12 09:50:23 -0600

    create context and add useEffect to switch from dark to light mode when the button is clicked

--- projects/src/projects/07-LightDarkMode/Blog.jsx
+++ projects/src/projects/07-LightDarkMode/Blog.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Title from '../components/Title'
 import Button from '../components/Button'
+import { ThemeContext } from './context/theme-context'
 
-const Blog = ({ theme }) => {
+const Blog = () => {
+  const {theme, changeTheme} = useContext(ThemeContext)
   return (
-    <div className='container'>
-        <Title text={`My Blog With ${theme} Theme`}/> 
-        <span style={{position: "absolute", top:10, right: 10}}>
-            <Button text={"theme"}/>
-        </span>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero iure odit ducimus corrupti quam, commodi inventore id ipsum non tenetur blanditiis, omnis ad voluptates sapiente reprehenderit sed fuga quaerat velit.</p>
-    </div>
-  )
+      <div className="container">
+          <Title text={`My Blog With ${theme} Theme`} />
+          <span style={{ position: "absolute", top: 10, right: 10 }}>
+              <Button
+                  text={theme === "dark" ? "Light" : "Dark"}
+                  btnClass={theme === "dark" && "btn-light"}
+                  onClick={changeTheme}
+              />
+          </span>
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+              iure odit ducimus corrupti quam, commodi inventore id ipsum non
+              tenetur blanditiis, omnis ad voluptates sapiente reprehenderit sed
+              fuga quaerat velit.
+          </p>
+      </div>
+  );
 }
 
 export default Blog
--- projects/src/projects/07-LightDarkMode/LightDarkMode.jsx
+++ projects/src/projects/07-LightDarkMode/LightDarkMode.jsx
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import Blog from "./Blog";
+import { ThemeContext, themes } from './context/theme-context'
 
 const LightDarkMode = () => {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState(themes.light)
+
+    const changeTheme = () => {
+        theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light)
+    }
+
+    let docBody = document.body
+    useEffect(() => {
+        switch(theme){
+            case themes.light:
+                docBody.classList.remove("bg-dark")
+                docBody.classList.remove("text-light")
+                docBody.classList.add("bg-light")
+                docBody.classList.add("text-dark")
+                break;
+            case themes.dark:
+                docBody.classList.remove("bg-light")
+                docBody.classList.remove("text-dark")
+                docBody.classList.add("bg-dark")
+                docBody.classList.add("text-light")
+                break;
+        }
+    }, [theme])
     return (
-        <div>
-            <Blog theme={"light"} />
-        </div>
+        <ThemeContext.Provider value={{theme, changeTheme}}>
+            <Blog />
+        </ThemeContext.Provider>
     );
 };
 
