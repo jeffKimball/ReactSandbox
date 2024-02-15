3221edc9ae136cbca5739cb233aab06ccab34778
J Kimball <jeffkimballis@gmail.com>
2024-02-15 08:45:35 -0600

    import react icon lock and add state and styles to slider UI

--- projects/src/projects/08-SlideToUnlock/SlideToUnlock.jsx
+++ projects/src/projects/08-SlideToUnlock/SlideToUnlock.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LockSlider from './LockSlider'
+import { AiFillUnlock } from "react-icons/ai"
 
 const SlideToUnlock = () => {
+  const [showLockSlider, setShowLockSlider] = useState(true)
   return (
-    <div>
-      <LockSlider />
+    <div className='container text-center d-flex flex-column border-20'
+    style={{
+      height:"70vh",
+      marginTop: "15vh",
+      width: 340,
+      border: "4px solid #000",      
+    }}>
+      <h1 className='title'>Title</h1>
+      {showLockSlider ? (<LockSlider width={"250px"}/>) :
+                        (<AiFillUnlock className='unlockIcon'/>)
+      }
+      
+      
     </div>
   )
 }
