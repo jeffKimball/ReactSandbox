ac6bb72f696ed62aca61b18485bdfc070cf4b966
J Kimball <jeffkimballis@gmail.com>
2024-02-19 11:34:50 -0600

    handle lock screen logic

--- projects/src/projects/08-SlideToUnlock/SlideToUnlock.jsx
+++ projects/src/projects/08-SlideToUnlock/SlideToUnlock.jsx
@@ -7,13 +7,21 @@ import HomeScreenImg from './images/home.jpg'
 
 
 const SlideToUnlock = () => {
+  // states
   const [uiProps, setUiProps] = useState({
     uiText: "Unlock Screen",
     uiColor: "#eee",
     uiBg: `url(${LockScreenImg}) center/cover`
   })
   const [showLockSlider, setShowLockSlider] = useState(true)
+  const [lockSliderValue, setLockSliderValue] = useState(0)
 
+  const handleLockSliderInput = (e) => {
+    setLockSliderValue(e.target.value)
+    setShowLockSlider(false)
+  }
+
+  // styles
   const containerStyle = {
     height: "70vh",
     marginTop: "15vh",
@@ -25,7 +33,7 @@ const SlideToUnlock = () => {
   return (
     <div className='container text-center d-flex flex-column border-20' style={containerStyle}>
       <h1 className='title' style={{color: uiProps.uiColor}}>{uiProps.uiText}</h1>
-      {showLockSlider ? <LockSlider width={"250px"}/> : <AiFillUnlock className='unlockIcon'/>}
+      {showLockSlider ? <LockSlider width={"250px"} handleInput={handleLockSliderInput} value={lockSliderValue} /> : <AiFillUnlock className='unlockIcon'/>}
     </div>
   )
 }
